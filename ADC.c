2f9d80da1602448674f66abc5a11f6231aa24e34
HadyP <a.hadi.1997@gmail.com>
2019-05-10 01:09:54 +0200

    Create ADC.c

---
+++ ADC.c
@@ -0,0 +1,13 @@
+#include "ADC.h"
+/**** steps
+* Enable control clock to ADC and GPIO Module by choosing which ADC mode
+* Choose & Disable the sample Sequencer
+* Choose the software Trigger
+* Select ADC INPUT or temperatur Sensor Mode
+* Set ADC control
+* if interrupt is used we will make a mask for it
+* configuration GPIO
+* Enable seq
+* Start a new conversion
+* WaiT & Read
+* ****/
