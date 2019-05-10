dafed2bd218e18ec6c43a2dd1a5b07d54d08c451
HadyP <a.hadi.1997@gmail.com>
2019-05-10 02:28:30 +0200

    add ADC_Init  function to ADC.c

--- ADC.c
+++ ADC.c
@@ -11,3 +11,21 @@
 * Start a new conversion
 * WaiT & Read
 * ****/
+void ADC_Init( enum ADCn adcN, enum SEQn seqN, uint32_t SEQ_SEL_DIS_MUSK, uint32_t which_trigger, uint32_t CH_MUSK, uint32_t CTL_MUSK )
+{
+		ADC_module(adcN);
+	if( adcN == 0 )
+		{
+			ADC0_selec_DIS_SEQ( SEQ_SEL_DIS_MUSK );
+			ADC0_select_Trigger( which_trigger );
+			ADC0_SEL_IN( CH_MUSK, seqN );
+			ADC0_SET_CTL( CTL_MUSK, seqN );
+		}
+		else if(adcN == 1)
+		{
+			ADC1_selec_DIS_SEQ(SEQ_SEL_DIS_MUSK);
+			ADC1_select_Trigger(which_trigger);
+			ADC1_SEL_IN( CH_MUSK, seqN );
+			ADC1_SET_CTL( CTL_MUSK, seqN );
+		}
+}
