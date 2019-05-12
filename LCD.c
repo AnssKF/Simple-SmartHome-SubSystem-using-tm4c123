0b0c7d7dbf657286d00c7c8f2423054f81765511
israasyz <israa.syz@yahoo.com>
2019-05-13 01:44:30 +0200

    Edit LCD.c

--- LCD.c
+++ LCD.c
@@ -1,5 +1,4 @@
 #include "LCD.h"
-
 void LCD_DisplayChar(uint8_t Data)
 {
 		/* RW  Should be 0  and RS should be 1 when Writing Data to LCD */
@@ -55,5 +54,59 @@ void LCD_DisplayChar(uint8_t Data)
 		CLEAR_BIT(LCD_Control_reg_OP,E);						
 		/* Data Hold delay Th = 10ns */
 		_delay_ms(1);
-		
+}
+void LCD_DisplayString(char * ptr)
+	{
+		while(*ptr != '\0')
+		{
+			LCD_DisplayChar(*ptr);
+			ptr++;
+		}
 	}
+
+void LCD_Clear(void)
+	{
+		/* Clear Screen */
+		Send_instruction(Clear_display); 
+	}
+
+void LCD_Select_RowCol(uint8_t Row , uint8_t col)
+	{
+		uint8_t Address=0;
+		
+		switch(Row)
+		{
+			case 0:
+					/* When Writing To LCD 1st Col The Base Address is 0x00 */
+						Address=col | LCD_1ST_BASE;
+						break;
+			case 1:
+					/* When Writing To LCD 2nd Col The Base Address is 0x40 */
+						Address=col | LCD_2ND_BASE;
+						break;
+			case 2:
+					/* When Writing To LCD 1st Col The Base Address is 0x10 */
+						Address = col | LCD_3RD_BASE;
+						break;
+			case 3:
+					/* When Writing To LCD 1st Col The Base Address is 0x50 */
+						Address=col| LCD_4TH_BASE;
+						break;		
+		}
+		
+		Send_instruction(DDRAM_CONST|Address);
+	}
+	
+void _delay_ms(unsigned long msec) {
+    unsigned long count; // declare count down variable
+ 
+    while (msec) {
+        count = 15913;   // 15913 for simulation and 11934 for the real board
+ 
+        while (count) {
+            count--;     // when counting down is finished, it takes 0.999875ms
+        }
+        msec--;          // count down the number of milliseconds you want
+    }
+}
+
