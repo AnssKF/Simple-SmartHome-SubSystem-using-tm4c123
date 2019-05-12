88554d2b248733b773d4a8de0003d1893212f3c8
rehamessameltagoury <36308055+rehamessameltagoury@users.noreply.github.com>
2019-05-12 22:34:33 +0200

    edit on adc.c

--- ADC.c
+++ ADC.c
@@ -100,3 +100,42 @@ void ADC1_SET_CTL(uint32_t CTL_MUSK, enum SEQn seqN)
       break;
     }
 }
+
+void ADC_GPIO_PIN_CONFIG(uint32_t PIN_MUSK ,int port){
+	switch(port) 
+		{
+			case 1:
+				{ //port B
+					GPIO_PORTB_AFSEL_R |=PIN_MUSK;//enable alternating function
+					GPIO_PORTB_DEN_R &=~PIN_MUSK; //diable digital mode
+					GPIO_PORTB_AMSEL_R |= PIN_MUSK; //enable analog function
+				}
+				break;
+			case 3:
+				{//port D
+				GPIO_PORTD_AFSEL_R |=PIN_MUSK;//enable alternating function
+				GPIO_PORTD_DEN_R &=~PIN_MUSK; //diable digital mode
+				GPIO_PORTD_AMSEL_R |= PIN_MUSK; //enable analog function
+				}
+				break;
+			case 4:
+				{ //port E
+				GPIO_PORTE_AFSEL_R |=PIN_MUSK;//enable alternating function
+				GPIO_PORTE_DEN_R &=~PIN_MUSK; //diable digital mode
+				GPIO_PORTE_AMSEL_R |= PIN_MUSK; //enable analog function
+				}
+				break;
+	}
+}
+
+
+void ADC0_TAKE_SAMPLE(uint32_t seq_musk)   
+{/* Start a new conversion */
+			ADC0_PSSI_R |= seq_musk;
+}
+
+void ADC1_TAKE_SAMPLE(uint32_t seq_musk)
+{/* Start a new conversion */
+	
+	ADC1_PSSI_R |= seq_musk;
+}
