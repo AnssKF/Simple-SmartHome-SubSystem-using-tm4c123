5d61d079e6f6cef36d8b85e4c42d164f83160a62
NadaGitHub <36456564+NadaGitHub@users.noreply.github.com>
2019-05-13 06:03:10 +0200

    Create UART.h

---
+++ UART.h
@@ -0,0 +1,29 @@
+// U0Rx (VCP receive) connected to PA0
+// U0Tx (VCP transmit) connected to PA1
+
+// standard ASCII symbols
+#define CR   0x0D
+#define LF   0x0A
+#define BS   0x08
+#define ESC  0x1B
+#define SP   0x20
+#define DEL  0x7F
+
+void UART_Init(void);
+
+char UART_InChar(void);
+
+void UART_OutChar(char data);
+
+void UART_OutString(char* pt);
+
+uint32_t UART_InUDec(void);
+
+void UART_OutUDec(uint32_t n);
+
+uint32_t UART_InUHex(void);
+
+void UART_OutUHex(uint32_t number);
+
+void UART_InString(char* bufPt, uint16_t max);
+
